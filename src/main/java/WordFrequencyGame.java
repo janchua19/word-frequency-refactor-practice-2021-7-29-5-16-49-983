d40f6d7b6fbc0af2808a7b628c796c09f64348c2
janchua19 <jan.edward.chua@oocl.com>
2021-08-09 18:27:42 +0800

    [WordFrequencyGame][Jan] refactor: rename 'list' to 'uniqueWordInfos'

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -24,12 +24,12 @@ public class WordFrequencyGame {
                 //get the map for the next step of sizing the same word
                 Map<String, List<WordInfo>> map =getListMap(wordInfos);
 
-                List<WordInfo> list = new ArrayList<>();
+                List<WordInfo> uniqueWordInfos = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
                     WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
+                    uniqueWordInfos.add(wordInfo);
                 }
-                wordInfos = list;
+                wordInfos = uniqueWordInfos;
 
                 wordInfos.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
