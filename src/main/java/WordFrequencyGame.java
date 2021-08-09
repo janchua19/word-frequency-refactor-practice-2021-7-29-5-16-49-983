29522b97acb4ff67fcfcadbf1cde36114e8df689
janchua19 <jan.edward.chua@oocl.com>
2021-08-09 18:23:56 +0800

    [WordFrequencyGame][Jan] refactor: rename 's' to 'word'

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -16,8 +16,8 @@ public class WordFrequencyGame {
                 String[] words = inputStr.split(BLANK_SPACES);
 
                 List<Input> wordInfos = new ArrayList<>();
-                for (String s : words) {
-                    Input input = new Input(s, 1);
+                for (String word : words) {
+                    Input input = new Input(word, 1);
                     wordInfos.add(input);
                 }
 
